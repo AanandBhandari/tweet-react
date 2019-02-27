1208b54c69734a90bd39a6d0503d4ce9e602ff5b
unknown <aanandbhandari143@gmail.com>
2019-02-27 17:01:45 +0545

    added Proptypes validation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import moment from 'moment';
+import PropTypes from 'prop-types'
 
 function Avatar({hash}) { 
     var url = `https://www.gravatar.com/avatar/${hash}`;
@@ -10,6 +11,9 @@ function Avatar({hash}) {
          className="avatar" 
          alt="avatar" />
     )
+}
+Avatar.propTypes = {
+    hash : PropTypes.string.isRequired
 }
 
 function Message({message}) {
@@ -28,6 +32,12 @@ function NameWithHandle({author}) {
             <span className="handle">@{handle}</span>
         </span>
     )
+}
+NameWithHandle.propTypes = {
+    author: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        handle:PropTypes.string.isRequired
+    }).isRequired
 }
 
 const Time = ({time}) => ( <span className="time">{moment(time).fromNow()}</span> );
@@ -52,12 +62,18 @@ const RetweetButton = ({count}) => {
         </span>
     )
 };
+RetweetButton.propTypes = {
+    count : PropTypes.number
+}
 
 const LikeButton = ({ count }) => (
     <span className="like-button"> 
     <i className="fa fa-heart"/> 
     {count > 0 && <span className="like-count"> {count} </span>}
     </span> );
+LikeButton.propTypes = {
+    count : PropTypes.number
+}
 const MoreOptionsButton = () => ( <i className="fa fa-ellipsis-h more-options-button"/> )
 
 function Tweet({tweet}) {
@@ -78,13 +94,16 @@ function Tweet({tweet}) {
         </div>
     )
 }
+Tweet.propTypes = {
+    tweet : PropTypes.object.isRequired
+}
 const testTweet = {
     message: "Something about cats.",
     gravatar: "xyz", 
-    author: { handle: "catperson", name: "IAMA Cat Person" }, 
+    author: { handle: "catPerson", name: "IAMA Cat Person" }, 
     likes: 2,
-    retweets: 2, 
-    timestamp: "2019-01-30 21:24:37"
+    retweets: 4, 
+    timestamp: "2019-02-14 21:24:37"
     // timestamp:"[2017, 01, 05]"
 }
 ReactDOM.render(<Tweet tweet={testTweet}/>,
